da765d7ea33564e07fbbec6c7542536ce2ee22f7
som7199 <somin71991@gmail.com>
2024-06-28 11:43:27 +0900

    raspi day07

--- homework/qt_ctrl.py
+++ homework/qt_ctrl.py
@@ -7,6 +7,8 @@ import time
 redPin = 17
 greenPin = 27
 bluePin = 19
+led = [redPin, greenPin, bluePin]
+
 trigPin = 24
 echoPin = 23
 pirPin = 22
@@ -29,15 +31,34 @@ class WindowClass(QMainWindow, form_class):
 		self.setupUi(self)
 
 		# 이벤트 함수 등록
-		self.ledBtnOn.clicked.connect(self.ledBtnOnFunction)
-		self.ledBtnOff.clicked.connect(self.ledBtnOffFunction)
+		for i in range(len(led)):
+			self.led[i].clicked.connect(self.ledBtnOffFunction)
+
+		self.ledRed.clicked.connect(self.ledRedOnFunction)
+		self.ledGreen.clicked.connect(self.ledGreenOnFunction)
+		self.ledBlue.clicked.connect(self.ledBlueOnFunction)
+
 		self.ultraBtn.clicked.connect(self.ultraBtnFunction)
 
-	def ledBtnOnFunction(self):
+	def ledRedOnFunction(self):
 		GPIO.output(redPin, False)
+		GPIO.output(bluePin, True)
+		GPIO.output(greenPin, True)
+
+	def ledGreenOnFunction(self):
+		GPIO.output(redPin, True)
+		GPIO.output(greenPin, False)
+		GPIO.output(bluePin, True)
+
+	def ledBlueOnFunction(self):
+		GPIO.output(redPin, True)
+		GPIO.output(greenPin, True)
+		GPIO.output(bluePin, False)
 
 	def ledBtnOffFunction(self):
 		GPIO.output(redPin, True)
+		GPIO.output(bluePin, True)
+		GPIO.output(greenPin, True)
 
 	# TODO : ultraBtn 클릭하면 ultraLabel.Text에 거리 띄워지게 하기
 	# 세그먼트에 거리 띄우기
