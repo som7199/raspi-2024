2f608a852430e9046264ccc17f8b84fa0891e260
som7199 <somin71991@gmail.com>
2024-06-28 16:48:32 +0900

    raspi day07 - todo:ultraStopBtn

--- homework/qt_ctrl.py
+++ homework/qt_ctrl.py
@@ -35,46 +35,45 @@ class WindowClass(QMainWindow, form_class):
 		self.ledBlue.clicked.connect(self.ledBlueOnFunction)
 		self.ledOff.clicked.connect(self.ledOffFunction)
 
-		#self.rdoPiezo.clicked.connect(self.rdoPiezoOnFunction)
 		self.rdoPiezo.toggled.connect(self.rdoPiezoToggleFunction)
-		self.ultraBtn.clicked.connect(self.ultraBtnFunction)
+		self.ultraStartBtn.clicked.connect(self.ultraStartBtnFunction)
+		self.ultraStopBtn.clicked.connect(self.ultraStopBtnFunction)
 
 		self.piezo_thread = None			# 피에조 부저 제어 스레드
 		self.piezo_running = False		# 피에조 부저의 동작 여부 제어 플래그
 
 	def ledRedOnFunction(self):
-		self.lblInfo.setText("RED ON")
+		self.lblLedInfo.setText("RED ON")
 		GPIO.output(redPin, False)
 		GPIO.output(bluePin, True)
 		GPIO.output(greenPin, True)
 
 	def ledGreenOnFunction(self):
-		self.lblInfo.setText("GREEN ON")
+		self.lblLedInfo.setText("GREEN ON")
 		GPIO.output(redPin, True)
 		GPIO.output(greenPin, False)
 		GPIO.output(bluePin, True)
 
 	def ledBlueOnFunction(self):
-		self.lblInfo.setText("BLUE ON")
+		self.lblLedInfo.setText("BLUE ON")
 		GPIO.output(redPin, True)
 		GPIO.output(greenPin, True)
 		GPIO.output(bluePin, False)
 
 	def ledOffFunction(self):
-		self.lblInfo.setText("LED OFF")
+		self.lblLedInfo.setText("LED OFF")
 		GPIO.output(redPin, True)
 		GPIO.output(bluePin, True)
 		GPIO.output(greenPin, True)
 
 	def rdoPiezoOnFunction(self):
 		self.piezo_running = True
-		self.lblInfo.setText("Buzzor ON!")
+		self.lblSound.setText("Buzzor ON!")
 		self.pwm = GPIO.PWM(piezoPin, 440)
 		self.pwm.start(90.0)
 
 		#[0도, 1, 2, 3, 4솔, 5, 6, 7]
 		scale = [262, 294, 330, 349, 392, 440, 494, 523]
-		#scale = [262, 330]
 		melody = [0, 0, 4, 4, 5, 5, 4, 3, 3, 2, 2, 1, 1, 0]
 
 		while self.piezo_running:
@@ -90,7 +89,7 @@ class WindowClass(QMainWindow, form_class):
 
 	def rdoPiezoOffFunction(self):
 		self.piezo_running = False
-		#self.lblInfo.setText("Buzzor OFF!")
+		self.lblSound.setText("Buzzor OFF!")
 		if self.pwm is not None:
 			self.pwm.stop()
 			self.pwm = None
@@ -103,16 +102,71 @@ class WindowClass(QMainWindow, form_class):
 		else:
 			self.rdoPiezoOffFunction()
 
+	def measure(self):
+		GPIO.output(trigPin, True)		# 10us 동안 high 레벨로 trigger 출력하여 초음파 발생 준비
+		time.sleep(0.00001)
+		GPIO.output(trigPin, False)
+		start = time.time()							# 현재 시간 저장
+
+		while GPIO.input(echoPin) == False:		# echo가 없으면
+			start = time.time()									# 현재 시간을 start 변수에 저장하고
+
+		while GPIO.input(echoPin) == True:		# echo가 있으면
+			stop = time.time()									# 현재 시간을 stop 변수에 저장
+
+		dlapsed = stop - start								# 걸린 시간을 구하고
+		distance = (dlapsed * 19000) / 2			# 초음파 속도를 이용해서 거리 계산
+		return distance
 
 	# TODO : ultraBtn 클릭하면 lcdNumber에 거리 띄우기
 	# distance < 20일 경우 빨간불/경고음 발생
-	def ultraBtnFunction(self):
+	def ultraStartBtnFunction(self):
 		self.lblDist.setText("Start measuring!")
-		count = 0
-		print("Start Checking Distance!")
-		while count <= 50:
-			self.lcdNumber.display(count)
-			count += 1
+		self.pwm = GPIO.PWM(piezoPin, 440)
+
+		while True:
+			distance = self.measure()
+			print("Distance : %2.f cm" %distance)
+			self.lcdNumber.display(distance)
+
+			GPIO.output(redPin, True)
+			GPIO.output(greenPin, True)
+			GPIO.output(bluePin, True)
+
+			if distance <= 5:
+				self.pwm.start(50)
+				self.pwm.ChangeFrequency(200)
+				time.sleep(0.1)
+				GPIO.output(redPin, False)
+				GPIO.output(greenPin, True)
+				GPIO.output(bluePin, True)
+				self.pwm.ChangeFrequency(400)
+				time.sleep(0.1)
+
+			elif distance <= 10:
+				self.pwm.ChangeFrequency(300)
+				time.sleep(0.3)
+				GPIO.output(redPin, True)
+				GPIO.output(greenPin, False)
+				GPIO.output(bluePin, True)
+				time.sleep(0.3)
+
+			elif distance <= 20:
+				self.pwm.ChangeFrequency(400)
+				GPIO.output(redPin, True)
+				GPIO.output(greenPin, True)
+				GPIO.output(bluePin, False)
+				time.sleep(0.6)
+
+			else:
+				self.pwm.stop()
+				GPIO.output(redPin, True)
+				GPIO.output(greenPin, True)
+				GPIO.output(bluePin, True)
+				time.sleep(1)
+
+	def ultraStopBtnFunction(self):
+		print("HI")
 
 if __name__ == "__main__":
 	app = QApplication(sys.argv)
